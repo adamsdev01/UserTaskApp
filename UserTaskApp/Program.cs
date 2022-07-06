296a9eb6f29ac14e8958250354ae5f201711cc49
Tiffany Adams <tadams03@mail.roosevelt.edu>
2022-07-06 09:18:14 -0500

    Scaffold Identity

--- UserTaskApp/Program.cs
+++ UserTaskApp/Program.cs
@@ -3,15 +3,25 @@ using Microsoft.EntityFrameworkCore;
 using UserTaskApp.Data;
 
 var builder = WebApplication.CreateBuilder(args);
+var connectionString = builder.Configuration.GetConnectionString("ApplicationUserContextConnection") ?? throw new InvalidOperationException("Connection string 'ApplicationUserContextConnection' not found.");
+
+builder.Services.AddDbContext<ApplicationUserContext>(options =>
+    options.UseSqlServer(connectionString));;
+
+builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+    .AddEntityFrameworkStores<ApplicationUserContext>();;
 
 // Add services to the container.
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlServer(connectionString));
+
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
 builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
     .AddEntityFrameworkStores<ApplicationDbContext>();
+
 builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
