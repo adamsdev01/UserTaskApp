bde1efc73f1eb8fc94aeb1aa337173f0431a1e4b
Tiffany Adams <tadams03@mail.roosevelt.edu>
2022-07-09 11:09:51 -0500

    Add role based authentication - super admins can manage user roles

--- UserTaskApp/Controllers/UserRolesController.cs
+++ UserTaskApp/Controllers/UserRolesController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Identity;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using UserTaskApp.Models;
@@ -6,6 +7,7 @@ using UserTaskApp.ViewModels;
 
 namespace UserTaskApp.Controllers
 {
+    [Authorize(Roles = "SuperAdmin")]
     public class UserRolesController : Controller
     {
         private readonly UserManager<ApplicationUser> _userManager;
